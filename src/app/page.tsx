f1bb0d86eb522232d5a5d5853c434a128d2487bc
rlagudtn <rlagudtn4510@naver.com>
2024-03-09 15:44:22 +0900

    feat(ui): Add shadcn table sample ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,16 @@
 
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu";
 export default function Home() {
   const [companies, setCompanies] = useState();
   const fetchCompanies = async () => {
@@ -9,13 +19,11 @@ export default function Home() {
     const data = await res.json();
     setCompanies(companies);
   };
-  useEffect(() => {
-    fetchCompanies();
-  }, []);
+  // useEffect(() => {
+  //   fetchCompanies();
+  // }, []);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Home
-    </main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
   );
 }
